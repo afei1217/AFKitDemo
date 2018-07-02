482a163c9e84f39df11f14b140d3efe4f63c04f1
chenyongfei <chenyongfei@bjdvt.com>
2018-07-02 16:53:34 +0800

    给AFKit库添加头文件

---
+++ AFKitDemo/AFKit/AFCategories/AFCategories.h
@@ -0,0 +1,14 @@
+//
+//  AFCategories.h
+//  AFKitDemo
+//
+//  Created by Void on 2018/7/2.
+//  Copyright © 2018年 afei. All rights reserved.
+//
+
+#ifndef AFCategories_h
+#define AFCategories_h
+
+#import "UIView+Frame.h"        // UIView 的一些关于坐标、大小的快捷属性
+
+#endif /* AFCategories_h */
---
+++ AFKitDemo/AFKit/AFKit.h
@@ -0,0 +1,17 @@
+//
+//  AFKit.h
+//  AFKitDemo
+//
+//  Created by Void on 2018/7/2.
+//  Copyright © 2018年 afei. All rights reserved.
+//
+
+#ifndef AFKit_h
+#define AFKit_h
+
+//MARK: 分类
+#import "AFCategories.h"
+
+
+
+#endif /* AFKit_h */
